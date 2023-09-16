09ce36cabd89d7a2bc5574f537aed680c07446ed
Billal Hossain <122000530+billal-cuse@users.noreply.github.com>
2023-09-16 10:53:48 +0000

    h

--- index.js
+++ index.js
@@ -9,23 +9,10 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-
-const upload = multer({
-    dest:'./uploads/'
+app.get('/',(req,res)=>{
+  res.send('billal')
 })
 
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname,'./index.html'));
-});
-
-
-app.post("/", upload.single("avatar") , (req, res) => {
-    const {name, email, roll} = req.body
-    console.log(name,email,roll)
-  res.sendFile(path.join(__dirname,'./index.html'));
-
-});
 console.log(process.env.billal)
 
 app.listen(process.env.port, () => {
